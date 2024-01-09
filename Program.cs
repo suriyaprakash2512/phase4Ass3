c0a5dfe1e5adeefd712d8660cc16f42531fc82ae
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2024-01-09 14:46:34 +0530

    ass3

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Today's Assessment");
+Console.WriteLine("welcome to my first jenkins project");
